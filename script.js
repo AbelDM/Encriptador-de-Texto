db354a5c2d1771648302317c9379c812f83d7f61
Abel DM <abeldaza1991@gmail.com>
2023-04-18 22:44:21 -0500

    Mejorando el javascript para alertas y validaciones en la entrada y salida de textos

--- script.js
+++ script.js
@@ -5,43 +5,52 @@
 // La letra "u" es convertida para "ufat"
 
 function encriptar() {
-  let texto = document.getElementById("texto1").value.toLowerCase();
+  let texto = document.getElementById("texto1").value.toLowerCase(); 
 
-  let textoCifrado = texto.replace(/e/gim, "enter");
-  textoCifrado = textoCifrado.replace(/i/gim, "imes");
-  textoCifrado = textoCifrado.replace(/a/gim, "ai");
-  textoCifrado = textoCifrado.replace(/o/gim, "ober");
-  textoCifrado = textoCifrado.replace(/u/gim, "ufat");
+  if (texto.length == 0 || /^\s+|\s+$/.test(texto)) {
+    alert("No ha escrito texto, por favor escríbe el texto que desea ENCRIPTAR");
+    return false;
+  } else {
+    let textoCifrado = texto.replace(/e/gim, "enter");
+    textoCifrado = textoCifrado.replace(/i/gim, "imes");
+    textoCifrado = textoCifrado.replace(/a/gim, "ai");
+    textoCifrado = textoCifrado.replace(/o/gim, "ober");
+    textoCifrado = textoCifrado.replace(/u/gim, "ufat");
 
-  document.getElementById("muneco").style.display = "none";
-  document.getElementById("msj1").style.display = "none";
-  document.getElementById("msj2").style.display = "none";
-  document.getElementById("msj3").innerHTML = textoCifrado;
-  document.getElementById("texto1").innerHTML = " ";
-  document.getElementById("copiar").style.display = "show";
-  document.getElementById("copiar").style.display = "inherit";
-
+    document.getElementById("muneco").style.display = "none";
+    document.getElementById("msj1").style.display = "none";
+    document.getElementById("msj2").style.display = "none";
+    document.getElementById("msj3").innerHTML = textoCifrado.charAt(0).toUpperCase().concat(textoCifrado.substring(1, textoCifrado.length));
+    document.getElementById("texto1").innerHTML = " ";
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+  }
 }
 
 function desencriptar() {
   let texto = document.getElementById("texto1").value.toLowerCase();
 
-  let textoCifrado = texto.replace(/enter/gim, "e");
-  textoCifrado = textoCifrado.replace(/imes/gim, "i");
-  textoCifrado = textoCifrado.replace(/ai/gim, "a");
-  textoCifrado = textoCifrado.replace(/ober/gim, "o");
-  textoCifrado = textoCifrado.replace(/ufat/gim, "u");
+  if (texto === "") {
+    alert(
+      "No ha escrito texto, por favor escríbe el texto que desea DESENCRIPTAR"
+    );
+    return false;
+  } else {
+    let textoCifrado = texto.replace(/enter/gim, "e");
+    textoCifrado = textoCifrado.replace(/imes/gim, "i");
+    textoCifrado = textoCifrado.replace(/ai/gim, "a");
+    textoCifrado = textoCifrado.replace(/ober/gim, "o");
+    textoCifrado = textoCifrado.replace(/ufat/gim, "u");
 
-  document.getElementById("muneco").style.display = "none";
-  document.getElementById("msj1").style.display = "none";
-  document.getElementById("msj2").style.display = "none";
-  document.getElementById("msj3").innerHTML = textoCifrado;
-  document.getElementById("texto1").innerHTML = " ";
-  document.getElementById("copiar").style.display = "show";
-  document.getElementById("copiar").style.display = "inherit";
-  
+    document.getElementById("muneco").style.display = "none";
+    document.getElementById("msj1").style.display = "none";
+    document.getElementById("msj2").style.display = "none";
+    document.getElementById("msj3").innerHTML = textoCifrado.charAt(0).toUpperCase().concat(textoCifrado.substring(1, textoCifrado.length));
+    document.getElementById("texto1").innerHTML = " ";
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+  }
 }
-
 // function copiar() {
 //   let contenido = document.getElementById('msj2');
 //   contenido.select();
@@ -49,15 +58,41 @@ function desencriptar() {
 //   alert("Se ha copiado con éxito!");
 // }
 
-function copyToClickBoard(){
-    let content = document.getElementById('msj3').innerHTML;
+function copyToClickBoard() {
+  let content = document.getElementById("msj3").innerHTML;
 
-    navigator.clipboard.writeText(content)
-        .then(() => {
-        alert("Se ha copiado con éxito!")
+  navigator.clipboard
+    .writeText(content)
+    .then(() => {
+      alert("Se ha copiado con éxito!");
     })
-        .catch(err => {
-        alert('Algo salió mal', err);
-    })
- 
+    .catch((err) => {
+      alert("Algo salió mal", err);
+    });
+}
+
+// VALIDAR SOLO LETRAS, YA QUE SOLO PIDEN TEXTO
+function soloLetras(e) {
+  key = e.keyCode || e.which;
+  tecla = String.fromCharCode(key).toLowerCase();
+  letras = " áéíóúabcdefghijklmnñopqrstuvwxyz";
+  especiales = [8, 37, 39, 46];
+
+  tecla_especial = false;
+  for (var i in especiales) {
+    if (key == especiales[i]) {
+      tecla_especial = true;
+      break;
+    }
+  }
+
+  if (letras.indexOf(tecla) == -1 && !tecla_especial) return false;
+}
+
+function limpia() {
+  var val = document.getElementById("texto1").value;
+  var tam = val.length;
+  for (i = 0; i < tam; i++) {
+    if (!isNaN(val[i])) document.getElementById("texto1").value == 0;
+  }
 }
